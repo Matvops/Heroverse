9b44e9d3f39b5a0a5ec9c34c3b311f0877f30aba
Matheus <matheusmartinscia@gmail.com>
2025-11-20 15:56:42 -0300

    Feat: Cria métodos para preencher dados do herói

--- service/HeroService.js
+++ service/HeroService.js
@@ -1,4 +1,4 @@
-import { ApiMain } from "./ApiMain";
+import { ApiMain } from "./ApiMain.js";
 
 window.addEventListener('load', () => {
 
@@ -7,10 +7,11 @@ window.addEventListener('load', () => {
     const emptyHero = {
         name: null,
         gender: null,
-        race: '????',
+        race: null,
         fullName: null,
         publisher: null,
         alignment: null,
+        image: null,
         powerstats: {
             intelligence: null,
             strength: null,
@@ -22,4 +23,40 @@ window.addEventListener('load', () => {
     };
 
     
+    async function getHeroData()
+    {
+
+        try {
+            
+            const urlQueryParams = window.location.search;
+            const params = new URLSearchParams(urlQueryParams);
+            const id = params.get('id');
+            
+            const hero = await api.getById(id);
+
+            fillHeroObject(hero);
+
+        } catch(error) {
+            console.error(error.message);
+        }
+
+    }
+
+    function fillHeroObject(hero) {
+        emptyHero.name = hero.name;
+        emptyHero.gender = hero.appearence?.gender ?? '????';
+        emptyHero.race = hero.appearence?.race ?? '????';
+        emptyHero.fullName = hero.biography.fullName ?? '????';
+        emptyHero.publisher = hero.biography.publisher ?? '????';
+        emptyHero.alignment = hero.biography.alignment ?? '????';
+        emptyHero.image = hero.images.md ?? '????';
+        emptyHero.powerstats.combat = hero.powerstats.combat ?? '????';
+        emptyHero.powerstats.durability = hero.powerstats.durability ?? '????';
+        emptyHero.powerstats.intelligence = hero.powerstats.intelligence ?? '????';
+        emptyHero.powerstats.power  = hero.powerstats.power ?? '????';
+        emptyHero.powerstats.speed = hero.powerstats.speed ?? '????';
+        emptyHero.powerstats.strength = hero.powerstats.strength ?? '????';
+    }
+
+    getHeroData();
 });
