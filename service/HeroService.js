ad9f5069c98c79dcfb4a830fa3bfb194e47ed80e
Matheus <matheusmartinscia@gmail.com>
2025-11-19 22:01:52 -0300

    Feat: Cria classe heroService

---
+++ service/HeroService.js
@@ -0,0 +1,25 @@
+import { ApiMain } from "./ApiMain";
+
+window.addEventListener('load', () => {
+
+    const api = new ApiMain();
+
+    const emptyHero = {
+        name: null,
+        gender: null,
+        race: '????',
+        fullName: null,
+        publisher: null,
+        alignment: null,
+        powerstats: {
+            intelligence: null,
+            strength: null,
+            speed: null,
+            durability: null,
+            power: null,
+            combat: null,
+        }
+    };
+
+    
+});
