95c89385e327c9e4f778bd8e7d56cfd65079b3b7
Matheus <matheusmartinscia@gmail.com>
2025-11-23 17:59:01 -0300

    Feat: Cria integração dos itens

--- service/HeroService.js
+++ service/HeroService.js
@@ -4,6 +4,20 @@ window.addEventListener('load', () => {
 
     const api = new ApiMain();
 
+    const name = document.querySelector('#name');
+    const gender = document.querySelector('#gender');
+    const race = document.querySelector('#race');
+    const fullName = document.querySelector('#fullname');
+    const publisher = document.querySelector('#publisher');
+    const alignment = document.querySelector('#alignment');
+    const image = document.querySelector('#hero');
+    const combat = document.querySelector('#combat');
+    const power = document.querySelector('#power');
+    const durability = document.querySelector('#durability');
+    const speed = document.querySelector('#speed');
+    const strength = document.querySelector('#strength');
+    const intelligence = document.querySelector('#intelligence');
+
     const emptyHero = {
         name: null,
         gender: null,
@@ -33,8 +47,11 @@ window.addEventListener('load', () => {
             const id = params.get('id');
             
             const hero = await api.getById(id);
-
+            console.log(hero);
             fillHeroObject(hero);
+
+            createElements();
+
 
         } catch(error) {
             console.error(error.message);
@@ -44,18 +61,37 @@ window.addEventListener('load', () => {
 
     function fillHeroObject(hero) {
         emptyHero.name = hero.name;
-        emptyHero.gender = hero.appearence?.gender ?? '????';
-        emptyHero.race = hero.appearence?.race ?? '????';
-        emptyHero.fullName = hero.biography.fullName ?? '????';
+        emptyHero.gender = hero.appearance?.gender ?? '????';
+        emptyHero.race = hero.appearance?.race ?? '????';
+        emptyHero.fullName = hero.biography.fullName === '' ? '????' : hero.biography.fullName;
         emptyHero.publisher = hero.biography.publisher ?? '????';
         emptyHero.alignment = hero.biography.alignment ?? '????';
-        emptyHero.image = hero.images.md ?? '????';
+        emptyHero.image = hero.images.lg ?? '????';
         emptyHero.powerstats.combat = hero.powerstats.combat ?? '????';
         emptyHero.powerstats.durability = hero.powerstats.durability ?? '????';
         emptyHero.powerstats.intelligence = hero.powerstats.intelligence ?? '????';
         emptyHero.powerstats.power  = hero.powerstats.power ?? '????';
         emptyHero.powerstats.speed = hero.powerstats.speed ?? '????';
         emptyHero.powerstats.strength = hero.powerstats.strength ?? '????';
+    }
+
+    function createElements() {
+
+        name.textContent += emptyHero.name;
+        race.textContent += emptyHero.race;
+        fullName.textContent += emptyHero.fullName;
+        gender.textContent += emptyHero.gender;
+        publisher.textContent += emptyHero.publisher;
+        alignment.textContent += emptyHero.alignment == 'good' ? 'Hero' : 'Vilain/Anti-hero';
+        image.setAttribute('src',  emptyHero.image);
+
+        combat.textContent += emptyHero.powerstats.combat;
+        durability.textContent += emptyHero.powerstats.durability;
+        intelligence.textContent += emptyHero.powerstats.intelligence;
+        power.textContent += emptyHero.powerstats.power;
+        speed.textContent += emptyHero.powerstats.speed;
+        strength.textContent += emptyHero.powerstats.strength;
+
     }
 
     getHeroData();
